3a6b5c6d262ad179209a7979de50823661c67a32
Alejandro Moltó <96083767+alejandromolto@users.noreply.github.com>
2025-05-17 16:22:00 +0100

    Update snake.asm
    Implemented the printing function displaying to the Memory-mapped I/O.

--- snake.asm
+++ snake.asm
@@ -1,11 +1,20 @@
 .data
-    xSnake: .word -1:256
-    ySnake: .word -1:256
-    xApple: .word 11
-    yApple: .word 7
-    snakeSize: .word 0
-    rastorage: .word 0
-	
+
+xSnake: .word -1:256
+ySnake: .word -1:256
+xApple: .word 11
+yApple: .word 7
+snakeSize: .word 0
+snakeDir: .word 100	# Starting direction is 'd' (100 in ASCII)
+
+rastorage: .word 0
+
+rcontrol: .word 0xFFFF0000
+rdata: .word 0xFFFF0004
+tcontrol: .word 0xFFFF0008
+tdata:.word 0xFFFF000C
+
+
 .text
 .globl main
 
@@ -37,6 +46,48 @@ jal print
 
 li $v0, 10
 syscall
+
+
+
+
+
+display: # Receives to $a0 the character it wants to display and it displays it.
+
+	writing_loop:
+	lw $t9, tcontrol
+	lw $t9, 0($t9)
+	andi $t9, $t9, 0x1
+	bne $t9, 1, writing_loop
+	
+	lw $t7, tdata
+	move $t9, $a0
+	sw $t9, 0($t7)
+	
+	jr $ra
+
+isApple: # Receives to $a0 and $a1 coords x and y. Receives to $a2 xApple direction and to $a3 the yApple direction. Returns to v0 wether or not is the apple.
+
+
+	lw $t0, 0($a2)	# Stores in t0 xApple
+	lw $t1, 0($a3)	# Stores in t1 yApple	
+
+	beq $a0, $t0, firstEqual	# If xCoordinates are equal, it jumps to the second comparison
+	
+	li $v0, 0	# If not, it returns 0
+	jr $ra	
+	
+		firstEqual:
+			
+			beq $a1, $t1, secondEqual # If xCoordinates are equal, it jumps to the return
+			
+			li $v0, 0	# If not, it returns 0
+			jr $ra				
+			
+			secondEqual:
+				
+				li $v0, 1 # As both coordinates are equal, it is the apple and therefore it returns 1
+				jr $ra
+
 
 isInSnake: # Receives to $a0 and $a1 coords x and y. Receives to $a2 xSnake (And ySnake). Receives to $a3 the length of the snake. Returns to v0 wether or not is part of the sneak and to v1 wether or not is the head.
 	
@@ -77,59 +128,89 @@ isInSnake: # Receives to $a0 and $a1 coords x and y. Receives to $a2 xSnake (And
 
 print: # This function receives to $a0 snakeX, to $a1 snakeY
 
-li $t4, 0
-li $t8, 0
+	li $t4, 0
+	li $t8, 0
 
-outerloop:
-	innerloop:
+	outerloop:
+		innerloop:
 
-		move $a0, $t8
-		move $a1, $t4
-		la $a2, xSnake
-		lw $a3, snakeSize
+			move $a0, $t8
+			move $a1, $t4
+			la $a2, xSnake
+			lw $a3, snakeSize
 		
-		sw $ra, rastorage
-		jal isInSnake
-		lw $ra, rastorage 	
+			sw $ra, rastorage
+			jal isInSnake
+			lw $ra, rastorage 	
 		
-		beq $v0, $zero, printPoint 
+			beq $v0, $zero, printElse
 		
-		printSnake:
-			li $t7, 1
-			beq $v1, $t7 , Head
-			li $a0, 0x6F	
-			li $v0, 11
-			syscall
-			j keeploop
-			
-			Head:
-				li $a0, 0x4F
-				li $v0, 11
-				syscall
-				j keeploop				
+			printSnake:
+				beq $v1, 1 , Head
 				
-		printPoint:
-			li $a0, 0x2E
-			li $v0, 11
-			syscall		
-			j keeploop
+				# Print 0x6F (o)
+				li $a0, 0x6F	
+				sw $ra, rastorage	
+				jal display
+				lw $ra, rastorage
+	
+				j keeploop
+			
+				Head:
+					# Print 0x4F (O)
+					li $a0, 0x4F
+					sw $ra, rastorage	
+					jal display
+					lw $ra, rastorage
+					
+					j keeploop				
+				
+			printElse:
+
+				move $a0, $t8
+				move $a1, $t4
+				la $a2, xApple
+				la $a3, yApple	
+						
+				sw $ra, rastorage	
+				jal isApple
+				lw $ra, rastorage
+				
+				beq $v0, $zero, point			
+				
+					# Print 'a' (a)
+					li $a0, 'a'
+					sw $ra, rastorage
+					jal display
+					lw $ra, rastorage									
+					j keeploop
+																																	
+				point:												
+				# Print 0x2E (.)
+				li $a0, 0x2E
+				sw $ra, rastorage	
+				jal display
+				lw $ra, rastorage
+				
+				j keeploop
 		
 		keeploop:
 		addi $t8, $t8, 1
-		li $t7, 16
-		bne $t8, $t7, innerloop
+		bne $t8, 16, innerloop
 	
-
+	
+	# Print '\n'
 	li $a0, '\n'
-	li $v0, 11
-	syscall
+	sw $ra, rastorage	
+	jal display
+	lw $ra, rastorage
 	
 	li $t8, 0		
 	addi $t4, $t4, 1
-	li $t7, 16
-	bne $t4, $t7, outerloop
+	bne $t4, 16, outerloop
 	
 	jr $ra
+
 
 getDir:
 	
